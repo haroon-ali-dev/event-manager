37c0f0e7c75b9edf6c41ffde996bc8ca5b86d70a
Miladepour <epour.milad@gmail.com>
2023-06-12 18:09:33 +0100

    catch errors

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -2,10 +2,13 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Users = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, error, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
     return <div>Loading ...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
   }
 
   return (
