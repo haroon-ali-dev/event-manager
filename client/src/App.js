401abb89ea5fb7d2df3350913c3f364b0f8d4b4d
Miladepour <epour.milad@gmail.com>
2023-06-12 07:13:05 +0100

    basic setup for test

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+// import { Route, Routes } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import User from "./components/User";
 
-import About from "./pages/About";
-import Home from "./pages/Home";
 
-const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-	</Routes>
-);
+const App = () => {
+  const { isAuthenticated } = useAuth0();
+
+  return (
+    <div className="App">
+      {!isAuthenticated ? (
+        <div>
+          <p style={{ fontSize: "1.5rem" }}>Please Login.</p>
+          <LoginButton />
+        </div>
+      ) : (
+        <div>
+          <LogoutButton />
+          <User />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
