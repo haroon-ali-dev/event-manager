bc321217f7521ba4e6b129981bb7477f99fe34a5
Miladepour <epour.milad@gmail.com>
2023-06-12 18:22:51 +0100

    add bootstrap style to logout btn

--- client/src/components/LogoutButton.js
+++ client/src/components/LogoutButton.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
-      Log Out
-    </button>
+    <Container>
+      <Row>
+        <Col>
+          <Button onClick={() => logout({ returnTo: window.location.origin })} variant="primary">
+          Log Out
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
