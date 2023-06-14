8ac9959cdb3422c376ab3527441ad7628c53d0d2
Haroon <haroon.ali.1104@gmail.com>
2023-06-14 16:37:18 +0100

    fetch members data and display in console

--- client/src/pages/Members.js
+++ client/src/pages/Members.js
@@ -1,7 +1,38 @@
 import React from "react";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { useState, useEffect } from "react";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
 const Members = () => {
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    async function getMembers() {
+      try {
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: process.env.NODE_ENV === "development" ? "http://localhost:3000/api/" : "",
+          },
+        });
+
+        const res = await fetch("/api/members", {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const members = await res.json();
+        console.log(members);
+        setMembers(members);
+
+      } catch (e) {
+        console.log(e.message);
+      }
+    }
+
+    getMembers();
+  }, []);
 
   return (
     <div>Members</div>
--- server/routes/members.js
+++ server/routes/members.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 import db from "../db";
+const { auth } = require('express-oauth2-jwt-bearer');
 
-router.get("/", async (req, res) => {
-  res.send("ok");
+const jwtCheck = auth({
+    audience: process.env.NODE_ENV === "development" ? "http://localhost:3000/api/" : "",
+    issuerBaseURL: 'https://dev-e5so586xmispuho0.us.auth0.com/',
+    tokenSigningAlg: 'RS256'
+});
+
+router.get("/", jwtCheck, async (req, res) => {
+    try {
+        const { rows } = await db.query("SELECT * FROM members");
+
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 module.exports = router;
