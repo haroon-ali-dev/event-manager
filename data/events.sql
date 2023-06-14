7ffc44c3ed10a1e8f3d55ea26700d6b7d7103710
Haroon <haroon.ali.1104@gmail.com>
2023-06-14 22:54:30 +0100

    fix SQL files

--- data/events.sql
+++ data/events.sql
@@ -6,16 +6,26 @@ create table events (
 	created_by VARCHAR(300) NOT NULL
 );
 
-insert into events (id, name, date, information, created_by) values (1, 'sem', '2023-04-29', 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+insert into events (name, date, information, created_by) values ('ultrices posuere cubilia', '2022-12-23', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.
 
-Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
 
-Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.', '01H2QQW92E62S7WH3JA44MTPDR');
-insert into events (id, name, date, information, created_by) values (2, 'metus aenean', '2022-06-20', 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.', '01H2QQW92F08Z69S13PFQ23K6P');
-insert into events (id, name, date, information, created_by) values (3, 'praesent blandit lacinia erat', '2022-10-07', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.
+Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'Katha Tyrwhitt');
 
-Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', '01H2QQW92GNXN0FGDGV726Y1SJ');
-insert into events (id, name, date, information, created_by) values (4, 'consequat morbi', '2022-10-29', 'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.', '01H2QQW92H5CKM8GDVWANM1Q3C');
-insert into events (id, name, date, information, created_by) values (5, 'parturient montes nascetur', '2022-08-09', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+insert into events (name, date, information, created_by) values ('bibendum felis sed interdum', '2023-04-06', 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
 
-Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', '01H2QQW92KYSAAS3TXEMA3NZMJ');
+Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 'Lebbie Champion');
+
+insert into events (name, date, information, created_by) values ('erat', '2022-12-03', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
+
+Sed ante. Vivamus tortor. Duis mattis egestas metus.', 'Niki Yglesia');
+
+insert into events (name, date, information, created_by) values ('nulla facilisi cras', '2023-05-19', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.
+
+Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
+
+Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.', 'Melisse Ivanisov');
+
+insert into events (name, date, information, created_by) values ('pede venenatis non sodales', '2023-03-17', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Bobbie Venton');
