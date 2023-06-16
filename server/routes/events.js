dd9fd0c6ab034c5d1de22a46abf2398507ee3294
Miladepour <epour.milad@gmail.com>
2023-06-16 16:00:01 +0100

    connect route to database for /GET request

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
 import db from "../db";
+const { auth } = require("express-oauth2-jwt-bearer");
 
-router.get("/", async (req, res) => {
-    res.json("ok");
+const jwtCheck = auth({
+    audience: process.env.NODE_ENV === "development" ? "http://localhost:3000/api/" : "",
+    issuerBaseURL: "https://dev-e5so586xmispuho0.us.auth0.com/",
+    tokenSigningAlg: "RS256",
+});
+router.get("/",jwtCheck, async (req, res) => {
+      try {
+          const { rows } = await db.query("SELECT * FROM events");
+
+          res.json(rows);
+      } catch (err) {
+          res.status(500).json({ error: err.message });
+      }
 });
 
 module.exports = router;
