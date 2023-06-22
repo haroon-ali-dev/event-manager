44b5e35bf68b19e474e3b6b81ef14ac5365e0852
Haroon <haroon.ali.1104@gmail.com>
2023-06-22 13:16:17 +0100

    change members.json and members.sql, add attendance table

---
+++ data/attendance.sql
@@ -0,0 +1,5 @@
+create table attendance (
+	id SERIAL PRIMARY KEY,
+	e_id INT NOT NULL,
+    u_id INT NOT NULL
+);
--- data/members.sql
+++ data/members.sql
@@ -1,5 +1,6 @@
 create table members (
 	id SERIAL PRIMARY KEY,
+	g_id VARCHAR(100) NOT NULL,
 	first_name VARCHAR(150) NOT NULL,
 	last_name VARCHAR(150) NOT NULL,
 	gender VARCHAR(50) NOT NULL,
@@ -13,16 +14,12 @@ create table members (
 	created_by VARCHAR(300) NOT NULL
 );
 
-insert into members (first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('Leigh', 'Shambrook', 'Male', '2022-08-03', 'Apt 1147', 'PO Box 29622', 'lshambrook0@sitemeter.com', 99999999999, 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
+insert into members (g_id, first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('01H3HG0917ZE91D5VA7MKWAD2A', 'Sawyere', 'Bache', 'Male', '2023-03-02', '18th Floor', 'PO Box 91067', 'sbache0@huffingtonpost.com', 99999999999, 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
 
-Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.', '2022-08-24', 'Leigh Shambrook');
+In congue. Etiam justo. Etiam pretium iaculis justo.', '2022-08-09', 'Sawyere Bache');
+insert into members (g_id, first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('01H3HG091BARNMZNKTZVPYFRJW', 'Elset', 'Farloe', 'Female', '2023-04-07', 'Apt 1712', 'Suite 87', 'efarloe1@discovery.com', 99999999999, 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.', '2022-12-18', 'Elset Farloe');
+insert into members (g_id, first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('01H3HG091E37XFPA1VX9M42690', 'Haskell', 'Vivien', 'Male', '2023-04-08', 'Room 736', 'Apt 634', 'hvivien2@jiathis.com', 99999999999, 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', '2022-06-26', 'Haskell Vivien');
+insert into members (g_id, first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('01H3HG091GQPBX8MTE9VNF32F3', 'Leeland', 'MacFadin', 'Male', '2023-02-27', 'PO Box 82658', 'Room 1190', 'lmacfadin3@ihg.com', 99999999999, 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', '2022-11-11', 'Leeland MacFadin');
+insert into members (g_id, first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('01H3HG091K8D2HW0MBGYXK6Y6Y', 'Max', 'Chamberlaine', 'Female', '2023-01-26', 'Apt 1296', 'Apt 1882', 'mchamberlaine4@tuttocitta.it', 99999999999, 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.
 
-insert into members (first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('Jard', 'Hairesnape', 'Male', '2022-12-22', 'Apt 1215', 'Room 171', 'jhairesnape1@yellowbook.com', 99999999999, 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.
-
-In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', '2023-02-16', 'Jard Hairesnape');
-
-insert into members (first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('Vevay', 'Beckmann', 'Female', '2023-03-15', '18th Floor', 'Apt 1153', 'vbeckmann2@prweb.com', 99999999999, 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', '2022-07-26', 'Vevay Beckmann');
-
-insert into members (first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('Naomi', 'Donisi', 'Female', '2022-10-03', 'Suite 90', 'Suite 99', 'ndonisi3@marketwatch.com', 99999999999, 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.', '2022-07-28', 'Naomi Donisi');
-
-insert into members (first_name, last_name, gender, date_of_birth, address, post_code, email, mobile, additional_info, member_since, created_by) values ('Glenn', 'Gilder', 'Female', '2023-04-12', '13th Floor', '20th Floor', 'ggilder4@gmpg.org', 99999999999, 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', '2023-01-26', 'Glenn Gilder');
+Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.', '2022-12-21', 'Max Chamberlaine');
