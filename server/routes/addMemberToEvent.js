ed3bf2a9959d888f8a8f5cbad41f567544f1d561
Haroon <haroon.ali.1104@gmail.com>
2023-06-23 15:36:06 +0100

    complete API route for adding members to events

--- server/routes/addMemberToEvent.js
+++ server/routes/addMemberToEvent.js
@@ -10,8 +10,34 @@ const jwtCheck = auth({
     tokenSigningAlg: 'RS256'
 });
 
-router.post("/", jwtCheck, async (req, res) => {
-    
+router.post("/", async (req, res) => {
+    try {
+        await validate(req.body);
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+
+    try {
+        let rs = await db.query("SELECT * FROM members WHERE g_id = $1", [req.body.memberId]);
+        if (rs.rowCount <= 0) return res.status(404).json({ message: "Member does not exist." });
+
+        const uId = rs.rows[0].id;
+
+        rs = await db.query("SELECT * FROM attendance WHERE u_id = $1", [uId]);
+        if (rs.rowCount > 0) return res.status(404).json({ message: "Member already exists in event." });
+
+        rs = await db.query(
+            "INSERT INTO attendance (e_id, u_id) VALUES ($1, $2) RETURNING *",
+            [
+                +req.body.eventId,
+                uId
+            ]
+        );
+
+        res.json({ message: "Member added to event." });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 module.exports = router;
--- server/validations/addMemberToEvent.js
+++ server/validations/addMemberToEvent.js
@@ -2,8 +2,8 @@ import * as yup from "yup";
 
 function validate(member) {
     const schema = yup.object({
-        memberId: yup.string().required().label("Member ID"),
-        eventId: yup.string().required().label("Event ID")
+        memberId: yup.string().min(3).max(100).required().label("Member ID"),
+        eventId: yup.number().required().label("Event ID")
     }).required();
 
     return schema.validate(member)
