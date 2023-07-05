391f8743e002ecfd8655ed589f9bae7ad7e37f41
Miladepour <epour.milad@gmail.com>
2023-07-05 07:37:06 +0100

    create an API

--- server/routes/attendance.js
+++ server/routes/attendance.js
@@ -29,4 +29,24 @@ router.get("/member/:id", async (req, res) => {
     }
 });
 
+router.get("/event/:id", async (req, res) => {
+    const memberId = +req.params.id;
+
+    try {
+        const query = `
+            SELECT e.name, e.date
+            FROM attendance a
+            INNER JOIN events e
+            ON a.e_id = e.id
+            WHERE a.u_id = $1
+        `;
+
+        const { rows } = await db.query(query, [memberId]);
+
+        res.json(rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
 module.exports = router;
