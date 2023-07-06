8be9a41b95b8f3db126497d69f826dbeda1fd3bc
Miladepour <epour.milad@gmail.com>
2023-07-06 10:34:52 +0100

    add css to dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -37,7 +37,7 @@ const Dashboard = () => {
 
   return (
     <>
-        <div>Upcoming Events</div>
+        <h1 className="heading">Upcoming Events</h1>
       <Card>
         {upcomingEvents && upcomingEvents.map((event) => (
           <>
