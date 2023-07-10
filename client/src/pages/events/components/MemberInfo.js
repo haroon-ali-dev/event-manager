388a317f18d5b6caa278c073a78016cf96343711
Haroon <haroon.ali.1104@gmail.com>
2023-07-10 15:11:00 +0100

    display member info in modal

--- client/src/pages/events/components/MemberInfo.js
+++ client/src/pages/events/components/MemberInfo.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Table, Accordion } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 import moment from "moment";
 
@@ -35,39 +35,54 @@ export default function MemberInfo({ showMemberInfoModal }) {
     }, []);
 
     return (
-        <Table striped bordered hover style={{ tableLayout: "fixed", wordWrap: "break-word" }}>
-            <thead>
-                <tr>
-                    <th>Member ID</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Gender</th>
-                    <th>Date of Birth</th>
-                    <th>Address</th>
-                    <th>Post Code</th>
-                    <th>Email</th>
-                    <th>Mobile</th>
-                    <th>Member Since</th>
-                    <th>Created By</th>
-                    <th>Additional Info</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{member["g_id"]}</td>
-                    <td>{member["first_name"]}</td>
-                    <td>{member["last_name"]}</td>
-                    <td>{member["gender"]}</td>
-                    <td>{moment(member["date_of_birth"]).utcOffset("+0100").format("DD-MM-YYYY")}</td>
-                    <td>{member["address"]}</td>
-                    <td>{member["post_code"]}</td>
-                    <td>{member["email"]}</td>
-                    <td>{member["mobile"]}</td>
-                    <td>{moment(member["member_since"]).utcOffset("+0100").format("DD-MM-YYYY")}</td>
-                    <td>{member["created_by"]}</td>
-                    <td>{member["additional_info"]}</td>
-                </tr>
-            </tbody>
-        </Table>
+        <>
+            <Table striped bordered hover style={{ tableLayout: "fixed", wordWrap: "break-word" }}>
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Gender</th>
+                        <th>Date of Birth</th>
+                        <th>Address</th>
+                        <th>Post Code</th>
+                        <th>Email</th>
+                        <th>Mobile</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{member["first_name"]}</td>
+                        <td>{member["last_name"]}</td>
+                        <td>{member["gender"]}</td>
+                        <td>{moment(member["date_of_birth"]).utcOffset("+0100").format("DD-MM-YYYY")}</td>
+                        <td>{member["address"]}</td>
+                        <td>{member["post_code"]}</td>
+                        <td>{member["email"]}</td>
+                        <td>{member["mobile"]}</td>
+                    </tr>
+                </tbody>
+            </Table>
+
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Member Since</Accordion.Header>
+                    <Accordion.Body>
+                        {moment(member["member_since"]).utcOffset("+0100").format("DD-MM-YYYY")}
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Created By</Accordion.Header>
+                    <Accordion.Body>
+                        {member["created_by"]}
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Additional Info</Accordion.Header>
+                    <Accordion.Body>
+                        {member["additional_info"]}
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+        </>
     );
 }
