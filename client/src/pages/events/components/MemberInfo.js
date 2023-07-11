151077e70d85d904cee296a322364ff253a146ca
Haroon <haroon.ali.1104@gmail.com>
2023-07-11 16:55:14 +0100

    add spinner to member info modal

--- client/src/pages/events/components/MemberInfo.js
+++ client/src/pages/events/components/MemberInfo.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from "react";
-import { Table, Accordion } from "react-bootstrap";
+import { Table, Accordion, Spinner } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
 import moment from "moment";
 
 export default function MemberInfo({ showMemberInfoModal }) {
     const { getAccessTokenSilently } = useAuth0();
 
+    const [loading, setLoading] = useState(false);
     const [member, setMember] = useState({});
 
     async function getMemberInfo() {
+        setLoading(true);
+
         try {
             const accessToken = await getAccessTokenSilently({
                 authorizationParams: {
@@ -27,6 +30,8 @@ export default function MemberInfo({ showMemberInfoModal }) {
 
         } catch (e) {
             console.log(e.message);
+        } finally {
+            setLoading(false);
         }
     }
 
@@ -36,53 +41,62 @@ export default function MemberInfo({ showMemberInfoModal }) {
 
     return (
         <>
-            <Table striped bordered hover style={{ tableLayout: "fixed", wordWrap: "break-word" }}>
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Gender</th>
-                        <th>Date of Birth</th>
-                        <th>Address</th>
-                        <th>Post Code</th>
-                        <th>Email</th>
-                        <th>Mobile</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{member["first_name"]}</td>
-                        <td>{member["last_name"]}</td>
-                        <td>{member["gender"]}</td>
-                        <td>{moment(member["date_of_birth"]).utcOffset("+0100").format("DD-MM-YYYY")}</td>
-                        <td>{member["address"]}</td>
-                        <td>{member["post_code"]}</td>
-                        <td>{member["email"]}</td>
-                        <td>{member["mobile"]}</td>
-                    </tr>
-                </tbody>
-            </Table>
+            {loading && (
+                <Spinner className="spinner-main" animation="border" role="status" size="lg">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            )}
+            {!loading && (
+                <>
+                    <Table striped bordered hover style={{ tableLayout: "fixed", wordWrap: "break-word" }}>
+                        <thead>
+                            <tr>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Gender</th>
+                                <th>Date of Birth</th>
+                                <th>Address</th>
+                                <th>Post Code</th>
+                                <th>Email</th>
+                                <th>Mobile</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{member["first_name"]}</td>
+                                <td>{member["last_name"]}</td>
+                                <td>{member["gender"]}</td>
+                                <td>{moment(member["date_of_birth"]).utcOffset("+0100").format("DD-MM-YYYY")}</td>
+                                <td>{member["address"]}</td>
+                                <td>{member["post_code"]}</td>
+                                <td>{member["email"]}</td>
+                                <td>{member["mobile"]}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
 
-            <Accordion>
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Member Since</Accordion.Header>
-                    <Accordion.Body>
-                        {moment(member["member_since"]).utcOffset("+0100").format("DD-MM-YYYY")}
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>Created By</Accordion.Header>
-                    <Accordion.Body>
-                        {member["created_by"]}
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="2">
-                    <Accordion.Header>Additional Info</Accordion.Header>
-                    <Accordion.Body>
-                        {member["additional_info"]}
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+                    <Accordion>
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Member Since</Accordion.Header>
+                            <Accordion.Body>
+                                {moment(member["member_since"]).utcOffset("+0100").format("DD-MM-YYYY")}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header>Created By</Accordion.Header>
+                            <Accordion.Body>
+                                {member["created_by"]}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header>Additional Info</Accordion.Header>
+                            <Accordion.Body>
+                                {member["additional_info"]}
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </>
+            )}
         </>
     );
 }
