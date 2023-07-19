df0eb9b4207f70238f2800dceaea1a6c409842cc
Haroon <haroon.ali.1104@gmail.com>
2023-07-19 13:55:30 +0100

    embed QR code image in email, change email text to HTML

--- server/routes/sendMail.js
+++ server/routes/sendMail.js
@@ -1,40 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const requestSource = require("../middlewares/requestSource");
 const QrCode = require("qrcode");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-router.post("/",requestSource, async (req, res) => {
-  const { body } = req;
-    const qrCode = await QrCode.toDataURL(body.data.g_id);
-    body.data.qrCode = qrCode;
-
-  const formatData = (data) => {
-    let result = "";
-    if (!data || typeof data !== "object") {
-      return result;
-    }
-    result += `Name: ${data.first_name} ${data.last_name} \nMembership ID: ${data.g_id}\n\n`;
-    return result;
-  };
-
-  const formattedData = formatData(body.data);
+router.post("/", requestSource, async (req, res) => {
+  const qrCode = await QrCode.toDataURL(body.data.g_id);
 
   const msg = {
     to: body.to,
     from: `Event Manager System <${process.env.SENDGRID_EMAIL}>`,
     subject: "Your Membership Information",
-    text: `Your Membership Information is as follows:\n\n${formattedData}\n\nThank you for your membership!`,
+    html: `
+      <p>Your Membership Information is as follows:</p>
+      <ul>
+        <li>Name: ${req.body.data.first_name} ${req.body.data.last_name}</li>
+        <li>Membership ID: ${req.body.data.g_id}</li>
+      </ul>
+      <img style="width:300px" src="cid:qrcode" alt="QR Code" />
+      <p>Thank you for your membership!s</p>
+    `,
     attachments: [
-        {
-          filename: "qrcode.png",
+      {
           content: qrCode.split(",")[1],
+          filename: "qr-code",
           type: "image/png",
-          disposition: "attachment",
-          contentId: "qrcode",
-        },
-      ],
+          content_id: "qrcode",
+          disposition: "inline"
+      }
+  ]
   };
 
   try {
