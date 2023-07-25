df756b71aa25aab737bf0cfa6be0829f50c91e5c
Miladepour <epour.milad@gmail.com>
2023-07-25 10:55:34 +0100

    add icons

--- client/src/components/sidebar/SideBar.js
+++ client/src/components/sidebar/SideBar.js
@@ -1,4 +1,5 @@
 import { Nav } from "react-bootstrap";
+import { House, People, Calendar } from "react-bootstrap-icons";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
@@ -13,9 +14,9 @@ function SideBar() {
       isAuthenticated && (
           <div className={styles.sidebar} >
               <Nav className="flex-column">
-                <Nav.Link to="/dashboard" as={Link}>Dashboard</Nav.Link>
-              <Nav.Link to="/members" as={Link}>Members</Nav.Link>
-              <Nav.Link to="/events" as={Link}>Events</Nav.Link>
+                <Nav.Link to="/dashboard" as={Link}><House  size={20} style={{ marginRight:"10px" }}></House> Dashboard</Nav.Link>
+              <Nav.Link to="/members" as={Link}><People size={20} style={{ marginRight:"10px" }}></People> Members</Nav.Link>
+              <Nav.Link to="/events" as={Link}><Calendar size={20} style={{ marginRight:"10px" }}></Calendar> Events</Nav.Link>
       </Nav>
     </div>
           )
