cadced3f88aee91e5af88afcf6521a066a290872
Haroon <haroon.ali.1104@gmail.com>
2023-08-15 16:52:26 +0100

    create e2e tests for member check-in for dashboard page

--- cypress/e2e/1-dashboard-page.cy.js
+++ cypress/e2e/1-dashboard-page.cy.js
@@ -17,3 +17,31 @@ describe('Events', () => {
     cy.contains("Max Chamberlaine").should('exist')
   })
 })
+
+describe('Member check-in', () => {
+  beforeEach(() => {
+    cy.task('seedDB');
+    cy.loginToAuth0(
+      Cypress.env('auth0_username'),
+      Cypress.env('auth0_password')
+    )
+    cy.visit('/dashboard');
+  });
+
+  it('Finds member', () => {
+    cy.contains('Check-in').click();
+    cy.get('#memberId').type('01H3HG0917ZE91D5VA7MKWAD2A');
+    cy.contains('Find').click();
+    cy.contains('Sawyere').should('exist');
+  })
+
+  it('Checks-in members', () => {
+    cy.contains('Check-in').click();
+    cy.get('#memberId').type('01H3HG0917ZE91D5VA7MKWAD2A');
+    cy.contains('Find').click();
+    cy.contains('Sawyere').should('exist');
+    cy.get('.btn-success').contains('Add').click();
+    cy.contains('Attended List').click();
+    cy.contains('Sawyere').should('exist');
+  })
+})
