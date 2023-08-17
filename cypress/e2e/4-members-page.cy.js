c5be1510f551f1f42ab1c28d1555295272085cb0
Miladepour <epour.milad@gmail.com>
2023-08-17 17:38:53 +0100

    form validation test

--- cypress/e2e/4-members-page.cy.js
+++ cypress/e2e/4-members-page.cy.js
@@ -17,3 +17,29 @@ describe('Display and search', () => {
         cy.get('#email').clear();
     })
   })
+
+  describe('Form validation', () => {
+    beforeEach(() => {
+      cy.task('seedDB');
+      cy.loginToAuth0(
+        Cypress.env('auth0_username'),
+        Cypress.env('auth0_password')
+      );
+      cy.visit('/members');
+      cy.contains('Add New').click();
+    });
+  
+    it('Required fields cannot be empty', () => {
+      cy.get('[type=submit]').contains('Add').click();
+      cy.contains('First Name must be at least 3 characters').should('exist');
+      cy.contains('Last Name must be at least 3 characters').should('exist');
+      cy.contains('Please select a gender.').should('exist');
+      cy.contains('Date of Birth must be a `date` type, but the final value was: `Invalid Date` (cast from the value `""`).').should('exist');
+      cy.contains('Address must be at least 3 characters').should('exist');
+      cy.contains('Email is a required field').should('exist');
+      cy.contains('Mobile must be at least 3 characters').should('exist');
+    });
+  });
+
+
+  
