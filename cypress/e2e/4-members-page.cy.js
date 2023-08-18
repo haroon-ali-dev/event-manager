12588a26e022ad3642dbe0ed3fa853c106646d61
Miladepour <epour.milad@gmail.com>
2023-08-18 17:03:44 +0100

    fail test

--- cypress/e2e/4-members-page.cy.js
+++ cypress/e2e/4-members-page.cy.js
@@ -1,3 +1,5 @@
+import { be } from "date-fns/locale";
+
 describe('Display and search', () => {
     beforeEach(() => {
       cy.task('seedDB');
@@ -39,7 +41,24 @@ describe('Display and search', () => {
       cy.contains('Email is a required field').should('exist');
       cy.contains('Mobile must be at least 3 characters').should('exist');
     });
+    it('Creates a new member', () => {
+        cy.get('#firstName').type('John');
+        cy.get('#lastName').type('Doe');
+        cy.get('#gender').select('Male');
+        cy.get('#dateOfBirth').type('1990-01-01');
+        cy.get('#address').type('123 Main St');
+        cy.get('#postCode').type('12345').last();
+        cy.get('#email').type('john.mike@gmail.com');
+        cy.get('#mobile').type('1234567890');
+        cy.get('#additionalInfo').type('Additional information about John Doe');
+        
+        cy.get('[type=submit]').contains('Add').click();
+        cy.contains('Member created.').should('exist');
+    
+        cy.contains('John Doe').should('exist');
+      });
+    
   });
-
+         
 
   
