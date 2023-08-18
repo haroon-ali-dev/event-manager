275bc065e55587425c6c9951b22fda54848f859c
Haroon <haroon.ali.1104@gmail.com>
2023-08-18 17:17:56 +0100

    fix

--- cypress/e2e/4-members-page.cy.js
+++ cypress/e2e/4-members-page.cy.js
@@ -1,24 +1,24 @@
 import { be } from "date-fns/locale";
 
-// describe('Display and search', () => {
-//   beforeEach(() => {
-//     cy.task('seedDB');
-//     cy.loginToAuth0(
-//       Cypress.env('auth0_username'),
-//       Cypress.env('auth0_password')
-//     )
-//     cy.visit('/members');
-//   });
+describe('Display and search', () => {
+  beforeEach(() => {
+    cy.task('seedDB');
+    cy.loginToAuth0(
+      Cypress.env('auth0_username'),
+      Cypress.env('auth0_password')
+    )
+    cy.visit('/members');
+  });
 
-//   it('Displays members', () => {
-//     cy.contains('01H3HG091BARNMZNKTZVPYFRJW').should('exist');
-//   })
-//   it('Searches members', () => {
-//     cy.get('#email').type('hvivien2@jiathis.com');
-//     cy.contains('01H3HG091E37XFPA1VX9M42690').should('exist');
-//     cy.get('#email').clear();
-//   })
-// })
+  it('Displays members', () => {
+    cy.contains('01H3HG091BARNMZNKTZVPYFRJW').should('exist');
+  })
+  it('Searches members', () => {
+    cy.get('#email').type('hvivien2@jiathis.com');
+    cy.contains('01H3HG091E37XFPA1VX9M42690').should('exist');
+    cy.get('#email').clear();
+  })
+})
 
 describe('Form validation', () => {
   beforeEach(() => {
@@ -31,16 +31,16 @@ describe('Form validation', () => {
     cy.contains('Add New').click();
   });
 
-  // it('Required fields cannot be empty', () => {
-  //   cy.get('[type=submit]').contains('Add').click();
-  //   cy.contains('First Name must be at least 3 characters').should('exist');
-  //   cy.contains('Last Name must be at least 3 characters').should('exist');
-  //   cy.contains('Please select a gender.').should('exist');
-  //   cy.contains('Date of Birth must be a `date` type, but the final value was: `Invalid Date` (cast from the value `""`).').should('exist');
-  //   cy.contains('Address must be at least 3 characters').should('exist');
-  //   cy.contains('Email is a required field').should('exist');
-  //   cy.contains('Mobile must be at least 3 characters').should('exist');
-  // });
+  it('Required fields cannot be empty', () => {
+    cy.get('[type=submit]').contains('Add').click();
+    cy.contains('First Name must be at least 3 characters').should('exist');
+    cy.contains('Last Name must be at least 3 characters').should('exist');
+    cy.contains('Please select a gender.').should('exist');
+    cy.contains('Date of Birth must be a `date` type, but the final value was: `Invalid Date` (cast from the value `""`).').should('exist');
+    cy.contains('Address must be at least 3 characters').should('exist');
+    cy.contains('Email is a required field').should('exist');
+    cy.contains('Mobile must be at least 3 characters').should('exist');
+  });
   it('Creates a new member', () => {
     cy.get('#firstName').type('John');
     cy.get('#lastName').type('Doe');
